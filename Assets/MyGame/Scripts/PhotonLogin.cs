768abe2a3cd75710703812905098e43c82233c9c
jenkeee <83080324+jenkeee@users.noreply.github.com>
2022-04-21 17:51:47 +0300

    dz3done
    кнопку не стал раскрашивать, подкрасил текст

--- Assets/MyGame/Scripts/PhotonLogin.cs
+++ Assets/MyGame/Scripts/PhotonLogin.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PhotonLogin : MonoBehaviourPunCallbacks
@@ -12,13 +14,15 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
     }
      void Start()
     {
-        Connect();
+       // Connect();
     }
 
     public void Connect()
     {
         if (PhotonNetwork.IsConnected)
+        {
             PhotonNetwork.JoinRandomRoom();
+        }
         else
         {
             PhotonNetwork.ConnectUsingSettings();
@@ -26,9 +30,40 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
         }
 
     }
+        public void DisConnect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+        }
+        else
+        {
+            Debug.LogError($"Photon is alredy disconnected: {PhotonNetwork.IsConnected}");
+            GetComponentInParent<TMP_Text>().text = "fail to connect";
+            GetComponentInParent<TMP_Text>().color = new Color32(147, 23, 27, 255);
+        }
+
+    }
 
     public override void OnConnected()
     {
         Debug.Log($"Photon callbacks | Congratulation, you made successful API call! {PhotonNetwork.CloudRegion }, ping: {PhotonNetwork.GetPing()}");
+        GetComponentInParent<TMP_Text>().text = "Connected";
+        GetComponentInParent<TMP_Text>().color = new Color32(8, 130, 0, 255);
     }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogError($"Something went wrong: {cause}");
+        GetComponentInParent<TMP_Text>().text = "fail to connect";
+        GetComponentInParent<TMP_Text>().color = new Color32(147, 23, 27, 255);
+    }
+
+    public override void OnCustomAuthenticationFailed(string messageDebug)
+    {
+        Debug.LogError($"Something went wrong: {messageDebug}");
+        GetComponentInParent<TMP_Text>().text = "fail to connect";
+        GetComponentInParent<TMP_Text>().color = new Color32(147, 23, 27, 255);
+    }
+    
 }
--- Assets/MyGame/Scripts/PlayFabLogin.cs
+++ Assets/MyGame/Scripts/PlayFabLogin.cs
@@ -1,11 +1,12 @@
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 using UnityEngine;
 
 public class PlayFabLogin : MonoBehaviour
 {
     // here we need to check whether TitledId property is configured in setting or not
-    void Start()
+   /* void Start()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
         {
@@ -17,16 +18,21 @@ public class PlayFabLogin : MonoBehaviour
         var request = new LoginWithCustomIDRequest { CustomId = "GB//Lesson3", CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
     }
-  
+  */
     private void OnLoginSuccess(LoginResult result)
-    { Debug.Log("Congratulation, you made successful API call!"); }
+    { Debug.Log("Congratulation, you made successful API call!");
+        GetComponentInParent<TMP_Text>().text = "Connected";
+        GetComponentInParent<TMP_Text>().color = new Color32(8, 130, 0, 255); 
+    }
     private void OnLoginFailure(PlayFabError error)
     {
         var errorMessage = error.GenerateErrorReport();
         Debug.LogError($"Something went wrong: {errorMessage}");
+        GetComponentInParent<TMP_Text>().text = "fail to connect";
+        GetComponentInParent<TMP_Text>().color = new Color32(147, 23, 27, 255); 
     }
 
-    private void ConnectPlayFab()
+    public void ConnectPlayFab()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
         {
@@ -37,5 +43,13 @@ public class PlayFabLogin : MonoBehaviour
 
         var request = new LoginWithCustomIDRequest { CustomId = "GB//Lesson3", CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    public void DisconnectPlayFab()
+    {
+        PlayFabClientAPI.ForgetAllCredentials();
+        Debug.LogError("PlayFabClientAPI.ForgetAllCredentials();");
+        GetComponentInParent<TMP_Text>().text = "fail to connect";
+        GetComponentInParent<TMP_Text>().color = new Color32(147, 23, 27, 255);
     }
 }
